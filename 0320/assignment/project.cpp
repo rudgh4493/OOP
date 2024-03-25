5e29176cc11a740943a16dae266d28b8600f183b
rudgh4493 <70314961+rudgh4493@users.noreply.github.com>
2024-03-25 23:49:55 +0900

    Add files via upload

---
+++ 0320/assignment/project.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cout << "덧셈, 뺄셈 문자열을 입력하세요" << endl;
+    getline(cin, s, '\n');
+
+    int sum = 0;
+    int startIndex = 0;
+    bool isPositive = true;
+
+    while (true) {
+        int fIndex = s.find_first_of("+-", startIndex);
+        if (fIndex == -1) {
+            string part = s.substr(startIndex);
+            if (part == "") break;
+            if (isPositive)
+                sum += stoi(part);
+            else
+                sum -= stoi(part);
+            break;
+        }
+        int count = fIndex - startIndex;
+        string part = s.substr(startIndex, count);
+
+        if (isPositive)
+            sum += stoi(part);
+        else
+            sum -= stoi(part);
+
+        if (s[fIndex] == '-')
+            isPositive = false;
+        else if (s[fIndex] == '+')
+            isPositive = true;
+
+        startIndex = fIndex + 1;
+    }
+    cout << "식의 답은? : " << sum;
+}
